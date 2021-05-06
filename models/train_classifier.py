8c7bd4b2ce32196b69a6fcbb87afc60b51c44f1d
David Orn <djohannesson@mining3.com>
2021-05-06 21:07:24 +1000

    Successfully ran a ML development pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,132 @@
 import sys
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy  as np
 
+# Natrual language tool kit
+import nltk
+import re
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk import pos_tag
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier   # Tried : Really slow in predict??
+
+# f1 score
+from sklearn.metrics import classification_report, precision_score, recall_score
+# Model improvement
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 def load_data(database_filepath):
-    pass
+    """Load the database previously created
+    returns
+      X    : training values (text messages)
+      Y    : categorical variable labels
+      categ: Names of the categorical values
+    """
+    engine = create_engine("sqlite:///" + database_filepath)
+    df = pd.read_sql_table("frame1", engine)
+    return df.message.values, df[df.columns[4:]], df.columns[4:]
 
 
 def tokenize(text):
-    pass
+    """A functional composition of using
+    - Regex removal of non character or numeric
+    - tokenizing the series using nltk
+    - Removing stop words
+    - Tagging the words
+    - Lemmatizing the words
+    - Stemming the words
+    """
+    print("nltk config...")
+    nltk.download('wordnet')
+    nltk.download('punkt')
+    nltk.download('stopwords')
+    nltk.download('averaged_perceptron_tagger')
+
+    def clean(text):
+        return re.sub(r"[^a-zA-Z0-9]", " ", text)
+
+    def stopClean(textList):
+        for word in textList:
+            if word in stopwords.words("english"):
+                textList.remove(word)
+                return textList
+
+    def whitespace_tokenize(text):
+        return clean(text).lower().split()
+
+    def sentence_tokinization(text):
+        return sent_tokenize(text)
+
+    def tokenize_base(text):
+        return word_tokenize(text)
+
+    def tokenize_full(text):
+        return stopClean(tokenize_base(clean(text.lower())))
+
+    def tag_and_tokenize(text):
+        return pos_tag(tokenize_full(text))
+
+    def stem_tag_tokenize(text):
+        return pos_tag([PorterStemmer().stem(w) for w in tokenize_full(text)])
+
+    def stem_lem_data(tokens):
+        """Run stemming and lemmatization on the results returning
+        a new list"""
+        out = []
+        for word, type_ in tokens:
+            # First we lemmatize the work
+            w = WordNetLemmatizer().lemmatize(word)
+            # Then we stem the word
+            w = PorterStemmer().stem(w)
+            out.append((w, type_))
+        return out
+
+    return stem_lem_data(stopClean(tag_and_tokenize(clean(text))))
 
 
 def build_model():
-    pass
+    """
+    Build the ML model using the pipeline methodology
+    """
+    pipeline = Pipeline([
+        ("vect", CountVectorizer()),
+        ("tfidf", TfidfTransformer()),
+        ("class", MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Run evaluation metrics on the model
+    """
+    #TODO update this function
+    pred = model.predict(X_test)
+    for indx, column in enumerate(category_names):
+        print(column,
+              classification_report(Y_test[column].values,
+                                    pred[:, indx]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """Save the model"""
+    with open(model_filepath, "wb") as fout:
+        pickle.dump(model, fout)
 
 
 def main():
@@ -47,7 +155,6 @@ def main():
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
-
 
 if __name__ == '__main__':
     main()
