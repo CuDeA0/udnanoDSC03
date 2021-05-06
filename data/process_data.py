9cec2aa93d60bb3eddcea52039de29cc8be8c7f7
David Orn <djohannesson@mining3.com>
2021-05-07 00:20:40 +1000

    Now we replace an update on the databse

--- data/process_data.py
+++ data/process_data.py
@@ -55,7 +55,7 @@ def save_data(df, database_filename):
     Save the pandas dataframe into an sqlite datafile
     """
     engine = create_engine("sqlite:///" + database_filename)
-    df.to_sql("frame1", engine, index=False)
+    df.to_sql("frame1", engine, index=False, if_exists="replace")
 
 
 def main():
